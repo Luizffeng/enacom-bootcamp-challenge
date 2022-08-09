fbe33cba6fb7e4ad6dab080e4253996ff0c52057
LuizFariaWeduu <luiz.faria@weduu.com>
2022-08-09 11:43:55 -0300

    Att Dynamic Recursion, att main

--- dynamic_recursion.py
+++ dynamic_recursion.py
@@ -19,30 +19,50 @@ def items_to_table(items: List[DataClass]) -> pd.DataFrame:
     return pd.DataFrame.from_records(records)
 
 
-def solution(case_array: list, orcamento: int, mmc=1) -> list:
+def solution(case_array: list, mmc=1) -> list:
 
     for case in case_array:
-        _orcamento = int(case[1]/mmc)
+        _orcamento = case[1]/mmc
+        print(_orcamento)
         _df = case[0]
+        _df['Custo'], _df['Retorno'] = _df['Custo'].div(mmc), _df['Retorno'].div(mmc)
         print(_df)
         
-        value_matrix = np.zeros((_orcamento + 1, _df.shape[0] + 1), int)
+        value_matrix = np.zeros((int(_orcamento) + 1, _df.shape[0] + 1), int)
         binary_matrix = value_matrix.copy()
 
         options = []
 
         #print(df.shape)
-        for i in range(1, _orcamento):
-            for j in range(1, _df.shape[0]):
-                if _df.iloc[j-1]['Retorno'] <= i:
-                    new_value = _df[j-1]['Custo'] + value_matrix[i - _df[0][j - 1]]
+        for i in range(1, int(_orcamento + 1)):
+            binary_matrix *= 0
+            for j in range(1, _df.shape[0] + 1):
+                custo = _df.iloc[j-1]['Custo']
+                retorno = _df.iloc[j-1]['Retorno']
+                
+                #print(custo, retorno, i, j, sep=' | ')
+                if custo <= i:
+                    new_value = retorno + value_matrix[i - int(custo)][j-1]
                     old_value = value_matrix[i][j-1]
-                    value_matrix = max(new_value, old_value)
+                    if new_value > old_value:
+                        binary_matrix[i][j] = 1 
+                    else:
+                        binary_matrix[i][j] = 0
+
+                    value_matrix[i][j] = max(new_value, old_value)
+                    #last = 
                 else:
                     value_matrix[i][j] = value_matrix[i][j-1]
+                
+            print(f'{i} = {value_matrix[i]} >< {binary_matrix[i]}')
+            #print(f'{i} = {value_matrix[i]}')
+
+        #print(binary_matrix)
+        print('>>>>', value_matrix[-1][-1])
 
 
-    print(value_matrix)
-    print(binary_matrix)
+
+    #print(value_matrix)
+    #print(binary_matrix)
 
     return 
--- main.py
+++ main.py
@@ -17,8 +17,10 @@ path = 'ENACOM Optimization Bootcamp - Desafio.pdf'
 df = Data.read(path).pdftodata()    # Lê o pdf e extrai os dados para um Dataframe
 df_and = df.copy()  # Faz um cópia do DF original para aplicar as restrições
 
+
 ### Rules
 orcamento = 10**6
+
 
 ### Restrictions
 soma_custo = df['Custo'].iloc[1] + df['Custo'].iloc[3]
@@ -26,12 +28,13 @@ soma_retorno = df['Retorno'].iloc[1] + df['Retorno'].iloc[3]
 df_and.loc[1]=['Opção 2+4', 'OPÇÃO AUXILIAR PARA REPRESENTAR OPÇÕES 2 E 4', soma_custo, soma_retorno]
 df_and = df_and.drop(3) # Dataframe contendo a opção 2+4
 
-table_1 = df_and.drop(4)   # Dataframe contendo a opção 1
-table_5 = df_and.drop(0)   # Dataframe contendo a opção 5
+table_1 = df_and.drop(4)   # Dataframe contendo a opção 1 e removendo a opção 5
+table_5 = df_and.drop(0)   # Dataframe contendo a opção 5 e removendo a opção 1
+
 
 # Array contendo os cases específicos
 # Vale lembrar que, separando o orçamento das demais 
-# restrições (cases), podemos também trabalhar com o mesmo
+# restrições (cases), podemos também trabalhar com diversos orçamentos por case
 #case_array = [[table_1, orcamento], [table_5, orcamento]] 
 case_array = [[table_1, orcamento]]
 
@@ -46,7 +49,7 @@ case_array = [[table_1, orcamento]]
 heuristic_solution = heuristic.solution(case_array, orcamento)
 
 # Heurística de recursão dinâmica
-dynamic_solution_0 = dynamic.solution(case_array, orcamento, 10000)
+dynamic_solution_0 = dynamic.solution(case_array, 10000)
 #dynamic_solution_1 = dynamic.solution(case_array[1], orcamento[0], 10000)
 
 # Algoritmo Genético
@@ -65,5 +68,5 @@ msg = (
     f'\nEsta solução consome {currency(heuristic_solution["Custo"])}'
     f' do orçamento e tem um retorno de {currency(heuristic_solution["Retorno"])}!!!'
 )
-print(msg)
+#print(msg)
 
